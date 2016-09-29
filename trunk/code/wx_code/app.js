8f77d8b53285ce046332508b131d7fa2d5a05e68
qizuwang123 <test@imactekiiMac.local>
2016-09-29 17:42:21 +0800

    初步倒入图片

--- trunk/code/wx_code/app.js
+++ trunk/code/wx_code/app.js
@@ -1,8 +1,3 @@
 //app.js
-const KIDSTONE_YELLOW = "#ff107"
 App({
-    // ========== 全局数据对象（整个应用程序共享） ==========
-  globalData: {
-    hasLogin: false,
-  }
 })
---
+++ trunk/code/wx_code/pages/index/index.js
@@ -0,0 +1,32 @@
+//index.js
+var image_path = "../../images/siri_750_1334.jpg"
+Page({
+  data:{
+    image_path:image_path
+  },
+
+   // onReady:function(e){
+   //     var context = wx.createContext();
+   //     context.drawImage(imagePath,0,0)
+   //     wx.drawCanvas({
+   //       canvasId:1,
+   //       actions:context.getActions()
+   //     });
+      //  var context = wx.createContext();
+      //  context.setFontSize(14)
+      //  context.fillText("MINA",50,50)
+      //  context.moveTo(0,50)
+      //  context.lineTo(100,50)
+      //  context.stroke();
+
+      //  context.setFontSize(20)
+      //  context.fillText("MINA",100,100)
+      //  context.moveTo(0,100)
+      //  context.lineTo(200,100)
+      //  context.stroke();
+      //  wx.drawCanvas({
+      //    canvasId:1,
+      //    actions:context.getActions()
+      //  });
+      // }
+   })
